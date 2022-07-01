18ba50f33b777e649737c713507802be68370c09
mrx-arafat <arafatmrx@gmail.com>
2022-07-01 07:50:21 +0600

    SQL

---
+++ SQL Command/data/User_table.sql
@@ -0,0 +1,19 @@
+
+
+INSERT INTO USER_TABLE (USER_ID, USERNAME, PASSWORD, DOB, DOR, ROLE, CONT_EMAIL, CONT_PHONE, ADD_COUNTRY, ADD_STATE, ADD_DISTRICT, ADD_CITY, ADD_AREA, ADD_POSTAL_CODE) 
+VALUES ('U_00001', 'Shadmanee Tasneem', 'abcd1234', TO_DATE('30 July 2000', 'DD Month, YYYY'), TO_DATE('12 Jan 2012', 'DD Month, YYYY'), 'Owner', 'shadmaneer@email.com', '+8801712347564', 'Bangladesh', 'Dhaka', 'Dhaka', 'Dhaka', 'Cantonment', '1206');
+
+INSERT INTO USER_TABLE (USER_ID, USERNAME, PASSWORD, DOB, DOR, ROLE, CONT_EMAIL, CONT_PHONE, ADD_COUNTRY, ADD_STATE, ADD_DISTRICT, ADD_CITY, ADD_AREA, ADD_POSTAL_CODE) 
+VALUES ('U_00002', 'Tasnim Ullah Shakib', 'ABCD1234', TO_DATE('14 November 2000', 'DD Month, YYYY'), TO_DATE('15 October 2021', 'DD Month, YYYY'), 'Customer', 'sakiber@email.com', '+8801712437564', 'Bangladesh', 'Dhaka', 'Dhaka', 'Dhaka', 'Banasree', '1219');
+
+INSERT INTO USER_TABLE (USER_ID, USERNAME, PASSWORD, DOB, DOR, ROLE, CONT_EMAIL, CONT_PHONE, ADD_COUNTRY, ADD_STATE, ADD_DISTRICT, ADD_CITY, ADD_AREA, ADD_POSTAL_CODE) 
+VALUES ('U_00003', 'Sadia Nur Nazifa', 'ABCD1234', TO_DATE('15 May 2000', 'DD Month, YYYY'), TO_DATE('1 November 2021', 'DD Month, YYYY'), 'Customer', 'sadiar@email.com', '+8801712437546', 'Bangladesh', 'Dhaka', 'Dhaka', 'Dhaka', 'Mohammadpur', '1207');
+
+
+INSERT INTO USER_TABLE (USER_ID, USERNAME, PASSWORD, DOB, DOR, ROLE, CONT_EMAIL, CONT_PHONE, ADD_COUNTRY, ADD_STATE, ADD_DISTRICT, ADD_CITY, ADD_AREA, ADD_POSTAL_CODE) 
+VALUES ('U_00004', 'Sadi Ahmed', 'ABCD1234', TO_DATE('15 May 1999', 'DD Month, YYYY'), TO_DATE('18 October 2021', 'DD Month, YYYY'), 'Customer', 'sadiasadi@email.com', '+8801719877564', 'Bangladesh', 'Dhaka', 'Dhaka', 'Dhaka', 'Aziz Polli', '1206');
+
+
+INSERT INTO USER_TABLE (USER_ID, USERNAME, PASSWORD, DOB, DOR, ROLE, CONT_EMAIL, CONT_PHONE, ADD_COUNTRY, ADD_STATE, ADD_DISTRICT, ADD_CITY, ADD_AREA, ADD_POSTAL_CODE) 
+VALUES ('U_00005', 'Will Byers', 'strang3rth1ngs', TO_DATE('20 December 1986', 'DD Month, YYYY'), TO_DATE('18 June 2020', 'DD Month, YYYY'), 'Owner', 'whereruwill@email.com', '+4471718777564', 'USA', 'California', 'District 1', 'Sacramento', 'Rosewood', '11209');
+
