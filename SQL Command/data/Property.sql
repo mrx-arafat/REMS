f19be25430343c13700b883f4fb6db1b6834ac2f
mrx-arafat <arafatmrx@gmail.com>
2022-07-01 15:44:42 +0600

    property title

--- SQL Command/data/Property.sql
+++ SQL Command/data/Property.sql
@@ -1,17 +1,17 @@
-INSERT INTO PROPERTY (PROPERTY_ID, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
-VALUES ('P_00001', 1, 3, 2, 'Hung Balcony', 'Attached', 'One-Wall Layout', 'Security Cameras on Property, Fire Extinguisher, Private Entrance, Free Parking, Pets Allowed', 'Residential', 2200, 5, 'For Rent', 24000, 'Alabama', 'Montgomery', 'District 3', 'Division 1', 'Available'); 
+INSERT INTO PROPERTY (PROPERTY_ID,P_TITLE, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
+VALUES ('P_00001','Apartment in Alabama', 1, 3, 2, 'Hung Balcony', 'Attached', 'One-Wall Layout', 'Security Cameras on Property, Fire Extinguisher, Private Entrance, Free Parking, Pets Allowed', 'Residential', 2200, 5, 'For Rent', 24000, 'Alabama', 'Montgomery', 'District 3', 'Division 1', 'Available'); 
 
 
-INSERT INTO PROPERTY (PROPERTY_ID, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
-VALUES ('P_00002', 3, 3, 1, 'Stacked Balcony', 'Attached', 'One-Wall Layout', 'Security Cameras on Property, Private Entrance, Free Parking, Backyard, Pets Allowed', 'Residential', 2400, 6, 'For Sale', 57000, 'California', 'Sacramento', 'District 1', 'Division 1', 'Available'); 
+INSERT INTO PROPERTY (PROPERTY_ID,P_TITLE, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
+VALUES ('P_00002','Apartment in Banani', 3, 3, 1, 'Stacked Balcony', 'Attached', 'One-Wall Layout', 'Security Cameras on Property, Private Entrance, Free Parking, Backyard, Pets Allowed', 'Residential', 2400, 6, 'For Sale', 57000, 'Banani', 'Sacramento', 'District 1', 'Division 1', 'Available'); 
 
-INSERT INTO PROPERTY (PROPERTY_ID, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
-VALUES ('P_00003', 16, 3, 2, 'False Balcony', 'Not Attached', 'L-Shaped Layout', 'Private Entrance, Free Parking, Backyard', 'Residential', 3200, 4, 'For Sale', 65000, 'California', 'Sacramento', 'District 1', 'Division 1', 'Available'); 
+INSERT INTO PROPERTY (PROPERTY_ID,P_TITLE, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
+VALUES ('P_00003','Apartment in Kochukhet', 16, 3, 2, 'False Balcony', 'Not Attached', 'L-Shaped Layout', 'Private Entrance, Free Parking, Backyard', 'Residential', 3200, 4, 'For Sale', 65000, 'Kochukhet', 'Sacramento', 'District 1', 'Division 1', 'Available'); 
 
 
 
-INSERT INTO PROPERTY (PROPERTY_ID, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
-VALUES ('P_00004', 20, 5, 2, 'Cantilevered Balcony', 'L-Shaped Layout', 'Any Type', 'Private Entrance, Free Parking', 'Residential', 14000, 8, 'For Rent', 15000, 'Berkeley', 'Perryridge', 'District 2', 'Division 7', 'Rented'); 
+INSERT INTO PROPERTY (PROPERTY_ID,P_TITLE, FLOORS, ROOMS_BEDROOMS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
+VALUES ('P_00004','Apartment in DOHS', 20, 5, 2, 'Cantilevered Balcony', 'L-Shaped Layout', 'Any Type', 'Private Entrance, Free Parking', 'Residential', 14000, 8, 'For Rent', 15000, 'Berkeley DOHS', 'Perryridge', 'District 2', 'Division 7', 'Rented'); 
 
-INSERT INTO PROPERTY (PROPERTY_ID, FLOORS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
-VALUES ('P_00005', 0, 20, 'Cantilevered Balcony', 'Peninsula Layout', 'Any Type', 'Free Parking', 'Commercial', 45000, 30, 'For Sale', 2000000, 'Berkeley', 'Perryridge', 'District 2', 'Division 7', 'Sold'); 
+INSERT INTO PROPERTY (PROPERTY_ID,P_TITLE, FLOORS, ROOMS_BATHROOMS, ROOMS_BALCONY, ROOMS_KITCHEN, ROOMS_DINING, FACILITIES, TYPE, PROPERTY_AREA, OCCUPANCY, PURPOSE, PRICE, LOC_AREA, LOC_CITY, LOC_DISTRICT, LOC_DIVISION, STATUS) 
+VALUES ('P_00005','Apartment in Grandarea', 0, 20, 'Cantilevered Balcony', 'Peninsula Layout', 'Any Type', 'Free Parking', 'Commercial', 45000, 30, 'For Sale', 2000000, 'Grandarea', 'Perryridge', 'District 2', 'Division 7', 'Sold'); 
